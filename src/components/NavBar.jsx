b374bca076e3f511d41d053eeee15e86fcd784b4
MichaelT-W23 <michael.totaro@icloud.com>
2025-03-13 22:27:23 -0400

    Style updates

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,16 +2,22 @@ import React from "react";
 import styles from "../styles/components/Navbar.module.css";
 import SearchBar from "./SearchBar";
 import { DepopLogo } from "../assets/svg/DepopLogo";
+import { LikeIcon } from "../assets/svg/Like";
+import { BagIcon } from "../assets/svg/Bag";
 
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
-      <div className={styles["navbar-title"]}>MyApp</div>
+      <div className={styles['depop-logo']}>
       <DepopLogo />
+      </div>
       <SearchBar />
       <div className={styles["navbar-controls"]}>
-        <button className={`${styles.button} ${styles["button-blue"]}`}>Button 1</button>
-        <button className={`${styles.button} ${styles["button-green"]}`}>Button 2</button>
+        <LikeIcon />
+        <BagIcon />
+        <button className={`${styles.button} ${styles["button-blue"]}`}>Sell now</button>
+        <button className={`${styles.button} ${styles["button-green"]}`}>Sign Up</button>
+        <p className={styles['log-in-text']}>Log in</p>
       </div>
     </nav>
   );
