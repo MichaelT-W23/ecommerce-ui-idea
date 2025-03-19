7575dad00bbfe2108eaeaddf7b34214433e99d2c
MichaelT-W23 <michael.totaro@icloud.com>
2025-03-19 00:15:56 -0400

    SideMenu Updates

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,6 +1,9 @@
 import { DepopLogo } from "../assets/svg/DepopLogo";
 import { CancelIcon } from "../assets/svg/Cancel";
+import { ArrowForwardIcon } from "../assets/svg/ArrowForward";
 import styles from "../styles/components/SideMenu.module.css";
+
+const menuItems = ["Men", "Women", "Kids", "Sports", "Brands", "Sale"];
 
 const SideMenu = ({ isOpen, setIsOpen }) => {
   return (
@@ -18,15 +21,22 @@ const SideMenu = ({ isOpen, setIsOpen }) => {
 
       {/* Buttons below the logo */}
       <div className={styles.menuButtons}>
-        <button className={`${styles.menuBtn} ${styles.sellNowBtn}`}>
-          Sell now
-        </button>
-        <button className={`${styles.menuBtn} ${styles.signUpBtn}`}>
-          Sign up
-        </button>
-        <button className={`${styles.menuBtn} ${styles.logInBtn}`}>
-          Log in
-        </button>
+        <button className={`${styles.menuBtn} ${styles.sellNowBtn}`}>Sell now</button>
+        <button className={`${styles.menuBtn} ${styles.signUpBtn}`}>Sign up</button>
+        <button className={`${styles.menuBtn} ${styles.logInBtn}`}>Log in</button>
+      </div>
+
+      {/* Gray line */}
+      <div className={styles.divider}></div>
+
+      {/* Menu List */}
+      <div className={styles.menuList}>
+        {menuItems.map((item, index) => (
+          <div key={index} className={styles.menuItem}>
+            <span>{item}</span>
+            <ArrowForwardIcon />
+          </div>
+        ))}
       </div>
     </div>
   );
